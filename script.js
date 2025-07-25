70226c27220c034ade26344f82541cef1fedc11d
khalidkarim-seo <khalidkarim.seo@gmail.com>
2025-07-26 03:02:22 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert("Thank you! Your message has been sent.");
-    this.reset();
-  });
-  
